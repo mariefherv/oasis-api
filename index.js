dd114319635010cc79e71bfc04b4d2a43496bd67
mariefherv <mzvillanueva1@up.edu.ph>
2023-08-19 13:21:00 +0800

    update cors

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ dotenv.config();
 
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({origin: 'https://oasis-black.vercel.app'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
